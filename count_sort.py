2a382b1cfb1e89dddedb4902eb53a103c9145188
oojo12 <47154698+oojo12@users.noreply.github.com>
2021-03-22 20:35:28 -0400

    Update count_sort.py

--- count_sort.py
+++ count_sort.py
@@ -13,18 +13,19 @@ def count_sort(arr, k):
     Output:
         sorted_arr - sorted array
     '''
-    
-    sorted_arr = [None]*len(arr)
-    temp_storage = [0]*(k+1)
+    # initialize 
+    sorted_arr = [None]*len(arr) # O(N)
+    temp_storage = [0]*(k+1) # O(K)
 
-    # build array of counts
+    # build array of counts O(N)
     for index in range(len(arr)):
         temp_storage[arr[index]] += 1
 
-    # update to number of elements below
+    # update to number of elements below O(K)
     for index in range(1, k+1):
         temp_storage[index] += temp_storage[index-1] 
 
+    # build sorted array O(N)
     for index in range(len(arr)-1, -1, -1):
         sorted_arr[temp_storage[arr[index]]-1] = arr[index]
         temp_storage[arr[index]] -= 1
